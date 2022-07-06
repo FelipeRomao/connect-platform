53e20444d773fd3d50362a62b34e39f1459fbbe2
FelipeRomao <feliperomao.a@gmail.com>
2022-07-06 13:37:50 -0300

    feat: improved class entity

--- src/shared/domain/entity.ts
+++ src/shared/domain/entity.ts
@@ -1,10 +1,12 @@
-import crypto from 'crypto';
+import { uuid } from 'uuidv4';
 
 export abstract class Entity<T> {
   public readonly id: string;
 
-  constructor(public readonly props: T) {
-    this.id = crypto.randomBytes(16).toString('hex');
+  constructor(public readonly props: Omit<T, 'id'>, id?: string) {
+    Object.assign(this, props);
+
+    if (!id) this.id = uuid();
   }
 
   toJSON() {
